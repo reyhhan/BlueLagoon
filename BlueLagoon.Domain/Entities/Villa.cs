7547ac8c3b88dcbf8b83a29c9a66ecef3a39aa60
reyhhan <reyhansara97@gmail.com>
2025-02-22 18:20:01 +0530

    Established DB connectivity

---
+++ BlueLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,15 @@
+﻿namespace BlueLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Updated_Date { get; set; }
+    }
+}
---
+++ BlueLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BlueLagoon.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueLagoon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "SunRay",
+                    Description = "Near the beach",
+                    ImageUrl = "",
+                    Occupancy = 4,
+                    Price = 10000,
+                    Sqft = 500
+                });
+        }
+    }
+}
