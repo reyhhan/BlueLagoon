2584b32b0a35eeb0d23efcd33b39199da0a8d405
reyhhan <reyhansara97@gmail.com>
2025-07-06 10:31:16 +0530

    feat - Added CRUD operations and Notifications

--- BlueLagoon.Domain/Entities/Villa.cs
+++ BlueLagoon.Domain/Entities/Villa.cs
@@ -1,12 +1,18 @@
-﻿namespace BlueLagoon.Domain.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlueLagoon.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [DisplayName("Price per night")]
+        [Range(10, 10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 10)]
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
         public DateTime Created_Date { get; set; }
--- BlueLagoon.Web/Controllers/VillaController.cs
+++ BlueLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using BlueLagoon.Infrastructure.Data;
+﻿using BlueLagoon.Domain.Entities;
+using BlueLagoon.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlueLagoon.Web.Controllers
@@ -14,6 +15,73 @@ namespace BlueLagoon.Web.Controllers
         {
             var villaList = _context.Villas.ToList();
             return View(villaList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("", "Name and Description cannot be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "The villa has been successfully created!";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "The villa could not be created!";
+            return View();
+            
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");       
+            }
+           
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {           
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _context.Villas.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "The villa has been successfully updated!";
+                return RedirectToAction("Index", "Villa");
+            }
+            TempData["error"] = "The villa could not be updated!";
+            return View();
+        }
+       
+    
+        public IActionResult Delete(int villaId)
+        {      
+            if (villaId > 0)
+            {
+                Villa? obj = _context.Villas.Find(villaId);
+                if (obj != null) 
+                {
+                    _context.Villas.Remove(obj);
+                    _context.SaveChanges();
+                    TempData["success"] = "The villa has been successfully deleted!";
+                    return RedirectToAction("Index", "Villa");
+                }
+                TempData["error"] = "The villa could not be deleted!";
+            }
+            return View();
         }
     }
 }
